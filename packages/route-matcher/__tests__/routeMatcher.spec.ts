98fefe797d4173714ce184cb62fdec854fec7c05
Thanga Ganapathy <ganapathy888@gmail.com>
2022-04-29 04:13:18 +0530

    Match static, dynamic & exact segment types

--- packages/route-matcher/__tests__/routeMatcher.spec.ts
+++ packages/route-matcher/__tests__/routeMatcher.spec.ts
@@ -4,167 +4,333 @@ describe('routeMatcher', () => {
   test('empty route', () => {
     expect(routeMatcher('', '')).toBe(null);
   });
+
   test('slash route with empty path', () => {
     expect(routeMatcher('/', '')).toBe(null);
   });
+
   test('slash route with slash path', () => {
-    expect(routeMatcher('/', '/')).toEqual([
-      { name: '', type: 'EXACT', value: '/' },
-    ]);
+    expect(routeMatcher('/', '/')).toEqual([{ type: 'EXACT', params: [] }]);
   });
+
   test('single static segment path', () => {
     expect(routeMatcher('/users', '/')).toBe(null);
     expect(routeMatcher('/users', '/u')).toBe(null);
     expect(routeMatcher('/users', '/user')).toBe(null);
     expect(routeMatcher('/users', '/users')).toEqual([
-      { name: '', type: 'EXACT', value: '/users' },
+      { type: 'EXACT', params: [] },
     ]);
     expect(routeMatcher('/users', '/Users')).toEqual([
-      { name: '', type: 'EXACT', value: '/Users' },
+      { type: 'EXACT', params: [] },
     ]);
   });
+
   test('two static segments path', () => {
     expect(routeMatcher('/users/new', '/users/new')).toEqual([
-      { name: '', type: 'EXACT', value: '/users/new' },
+      { type: 'EXACT', params: [] },
     ]);
   });
   test('multiple static segments path', () => {
     expect(
       routeMatcher('/docs/tutorials/intro', '/docs/tutorials/intro')
-    ).toEqual([{ name: '', type: 'EXACT', value: '/docs/tutorials/intro' }]);
+    ).toEqual([{ type: 'EXACT', params: [] }]);
   });
-  test('single dynamic segment path', () => {
+
+  test('single dynamic named segment path', () => {
     expect(routeMatcher('/:foo', '/')).toBe(null);
     expect(routeMatcher('/:foo', '/a')).toEqual([
-      { name: 'foo', type: 'DYNAMIC_NAMED', value: 'a' },
+      { type: 'DYNAMIC', params: [{ name: 'foo', value: 'a' }] },
     ]);
     expect(routeMatcher('/:foo', '/bar')).toEqual([
-      { name: 'foo', type: 'DYNAMIC_NAMED', value: 'bar' },
+      { type: 'DYNAMIC', params: [{ name: 'foo', value: 'bar' }] },
     ]);
     expect(routeMatcher('/:foo', '/bar-2')).toEqual([
-      { name: 'foo', type: 'DYNAMIC_NAMED', value: 'bar-2' },
+      { type: 'DYNAMIC', params: [{ name: 'foo', value: 'bar-2' }] },
     ]);
     expect(routeMatcher('/:foo', '/a/')).toBe(null);
     expect(routeMatcher('/:foo', '/a/b')).toBe(null);
   });
+
   test('two dynamic segments path', () => {
     expect(routeMatcher('/:foo/:bar', '/')).toBe(null);
     expect(routeMatcher('/:foo/:bar', '/a')).toBe(null);
     expect(routeMatcher('/:foo/:bar', '/a/')).toBe(null);
     expect(routeMatcher('/:foo/:bar', '/a/b')).toEqual([
-      { name: 'foo', type: 'DYNAMIC_NAMED', value: 'a' },
-      { name: 'bar', type: 'DYNAMIC_NAMED', value: 'b' },
+      { type: 'DYNAMIC', params: [{ name: 'foo', value: 'a' }] },
+      { type: 'DYNAMIC', params: [{ name: 'bar', value: 'b' }] },
     ]);
     expect(routeMatcher('/:foo/:bar', '/a/b/')).toBe(null);
     expect(routeMatcher('/:foo/:bar', '/a/b/c')).toBe(null);
   });
+
   test('multiple dynamic segments path', () => {
     expect(routeMatcher('/:foo/:bar/:baz', '/foo')).toBe(null);
     expect(routeMatcher('/:foo/:bar/:baz', '/foo/bar')).toBe(null);
     expect(routeMatcher('/:foo/:bar/:baz', '/docs/tutorials/intro')).toEqual([
-      { name: 'foo', type: 'DYNAMIC_NAMED', value: 'docs' },
-      { name: 'bar', type: 'DYNAMIC_NAMED', value: 'tutorials' },
-      { name: 'baz', type: 'DYNAMIC_NAMED', value: 'intro' },
+      { type: 'DYNAMIC', params: [{ name: 'foo', value: 'docs' }] },
+      { type: 'DYNAMIC', params: [{ name: 'bar', value: 'tutorials' }] },
+      { type: 'DYNAMIC', params: [{ name: 'baz', value: 'intro' }] },
     ]);
   });
+
   test('one static & one dynamic segments path', () => {
     expect(routeMatcher('/users/:id', '/users/1')).toEqual([
-      { name: '', type: 'STATIC', value: 'users' },
-      { name: 'id', type: 'DYNAMIC_NAMED', value: '1' },
+      { type: 'STATIC', params: [] },
+      { type: 'DYNAMIC', params: [{ name: 'id', value: '1' }] },
     ]);
     expect(routeMatcher('/users/:id', '/users/abc')).toEqual([
-      { name: '', type: 'STATIC', value: 'users' },
-      { name: 'id', type: 'DYNAMIC_NAMED', value: 'abc' },
+      { type: 'STATIC', params: [] },
+      { type: 'DYNAMIC', params: [{ name: 'id', value: 'abc' }] },
     ]);
   });
+
   test('mix static & dynamic segments', () => {
     expect(
       routeMatcher('/users/:username/posts/:postId', '/users/xxx/posts/1')
     ).toEqual([
-      { name: '', type: 'STATIC', value: 'users' },
-      { name: 'username', type: 'DYNAMIC_NAMED', value: 'xxx' },
-      { name: '', type: 'STATIC', value: 'posts' },
-      { name: 'postId', type: 'DYNAMIC_NAMED', value: '1' },
+      { type: 'STATIC', params: [] },
+      { type: 'DYNAMIC', params: [{ name: 'username', value: 'xxx' }] },
+      { type: 'STATIC', params: [] },
+      { type: 'DYNAMIC', params: [{ name: 'postId', value: '1' }] },
     ]);
   });
+
   test('dynamic named with custom regex segment', () => {
     expect(routeMatcher('/icon-:size(\\d+).png', '/icon-a.png')).toBe(null);
     expect(routeMatcher('/icon-:size(\\d+).png', '/icon-123.png')).toEqual([
-      { name: 'size', type: 'DYNAMIC_NAMED', value: '123' },
+      { type: 'DYNAMIC', params: [{ name: 'size', value: '123' }] },
     ]);
     expect(routeMatcher('/:orderId(\\d+)', '/250')).toEqual([
-      { name: 'orderId', type: 'DYNAMIC_NAMED', value: '250' },
+      { type: 'DYNAMIC', params: [{ name: 'orderId', value: '250' }] },
     ]);
     expect(routeMatcher('/:path(abc|xyz)', '/abc')).toEqual([
-      { name: 'path', type: 'DYNAMIC_NAMED', value: 'abc' },
+      { type: 'DYNAMIC', params: [{ name: 'path', value: 'abc' }] },
     ]);
     expect(routeMatcher('/:path(abc|xyz)', '/xyz')).toEqual([
-      { name: 'path', type: 'DYNAMIC_NAMED', value: 'xyz' },
+      { type: 'DYNAMIC', params: [{ name: 'path', value: 'xyz' }] },
     ]);
   });
-  test.only('dynamic unnamed with custom regex segments', () => {
+
+  test('dynamic unnamed with custom regex segments', () => {
     expect(routeMatcher('/(user|u)', '/user')).toEqual([
-      { name: '', type: 'DYNAMIC_UNNAMED', value: 'user' },
+      { type: 'DYNAMIC', params: [] },
     ]);
     expect(routeMatcher('/(user|u)', '/u')).toEqual([
-      { name: '', type: 'DYNAMIC_UNNAMED', value: 'u' },
+      { type: 'DYNAMIC', params: [] },
     ]);
     expect(routeMatcher('/(user|u)', '/users')).toBe(null);
     expect(routeMatcher('/(user|u)-(old|new)', '/user-old')).toEqual([
-      { name: '', type: 'DYNAMIC_UNNAMED', value: 'user-old' },
+      { type: 'DYNAMIC', params: [] },
     ]);
   });
+
   test('modifier: +', () => {
     expect(routeMatcher('/:chapters+', '/')).toBe(null);
     expect(routeMatcher('/:chapters+', '/one')).toEqual([
-      { name: 'chapters', type: 'DYNAMIC_NAMED', value: 'one' },
+      { type: 'DYNAMIC', params: [{ name: 'chapters', value: 'one' }] },
     ]);
     expect(routeMatcher('/:chapters+', '/one/two')).toEqual([
-      { name: 'chapters', type: 'DYNAMIC_NAMED', value: 'one/two' },
+      { type: 'DYNAMIC', params: [{ name: 'chapters', value: 'one/two' }] },
     ]);
     expect(routeMatcher('/:chapters+', '/one/two/three')).toEqual([
-      { name: 'chapters', type: 'DYNAMIC_NAMED', value: 'one/two/three' },
+      {
+        type: 'DYNAMIC',
+        params: [{ name: 'chapters', value: 'one/two/three' }],
+      },
     ]);
   });
+
   test('modifier: *', () => {
     expect(routeMatcher('/:foo*', '/')).toEqual([
-      { name: 'foo', type: 'DYNAMIC_NAMED', value: '/' },
+      {
+        type: 'DYNAMIC',
+        params: [{ name: 'foo', value: undefined }],
+      },
     ]);
     expect(routeMatcher('/:foo*', '/1')).toEqual([
-      { name: 'foo', type: 'DYNAMIC_NAMED', value: '/1' },
+      {
+        type: 'DYNAMIC',
+        params: [{ name: 'foo', value: '1' }],
+      },
     ]);
     expect(routeMatcher('/:foo*', '/one/two')).toEqual([
-      { name: 'foo', type: 'DYNAMIC_NAMED', value: '/one/two' },
+      {
+        type: 'DYNAMIC',
+        params: [{ name: 'foo', value: 'one/two' }],
+      },
     ]);
     expect(routeMatcher('/:foo*', '/one/two/three')).toEqual([
-      { name: 'foo', type: 'DYNAMIC_NAMED', value: '/one/two/three' },
+      {
+        type: 'DYNAMIC',
+        params: [{ name: 'foo', value: 'one/two/three' }],
+      },
     ]);
-    expect(routeMatcher('/:foo*', '/one/two/three/')).toEqual([
-      { name: 'foo', type: 'DYNAMIC_NAMED', value: '/one/two/three/' },
-    ]);
+    expect(routeMatcher('/:foo*', '/one/two/three/')).toEqual(null);
   });
+
   test('optional paramas', () => {
     expect(routeMatcher('/:foo/:bar?', '/foo')).toBe(null);
     expect(routeMatcher('/:foo/:bar?', '/foo/')).toEqual([
-      { name: 'foo', type: 'DYNAMIC_NAMED', value: 'foo' },
-      { name: 'bar', type: 'DYNAMIC_NAMED', value: undefined },
+      {
+        type: 'DYNAMIC',
+        params: [{ name: 'foo', value: 'foo' }],
+      },
+      {
+        type: 'DYNAMIC',
+        params: [{ name: 'bar', value: undefined }],
+      },
     ]);
     expect(routeMatcher('/:foo/:bar?', '/foo/bar')).toEqual([
-      { name: 'foo', type: 'DYNAMIC_NAMED', value: 'foo' },
-      { name: 'bar', type: 'DYNAMIC_NAMED', value: 'bar' },
+      {
+        type: 'DYNAMIC',
+        params: [{ name: 'foo', value: 'foo' }],
+      },
+      {
+        type: 'DYNAMIC',
+        params: [{ name: 'bar', value: 'bar' }],
+      },
     ]);
     expect(routeMatcher('/:foo/:bar?', '/foo/bar/')).toBe(null);
     expect(routeMatcher('/:foo/:bar?', '/foo/bar/baz')).toBe(null);
   });
+
   test('dynamic named in between static segments', () => {
     expect(routeMatcher('/teams/:teamId/edit', '/teams/team1/edit')).toEqual([
-      { name: '', type: 'STATIC', value: 'teams' },
-      { name: 'teamId', type: 'DYNAMIC_NAMED', value: 'team1' },
-      { name: '', type: 'STATIC', value: 'edit' },
+      {
+        type: 'STATIC',
+        params: [],
+      },
+      {
+        type: 'DYNAMIC',
+        params: [{ name: 'teamId', value: 'team1' }],
+      },
+      {
+        type: 'STATIC',
+        params: [],
+      },
     ]);
   });
-  test('star segments', () => {
+
+  test('invalid modifier', () => {
     expect(() => routeMatcher('/*', '/')).toThrow();
     expect(() => routeMatcher('/*', '/a')).toThrow();
+    expect(() => routeMatcher('/+', '/a')).toThrow();
+    expect(() => routeMatcher('/?', '/a')).toThrow();
+  });
+
+  test('no capturing groups inside custom regexp', () => {
+    expect(() => routeMatcher('/((.))', '/')).toThrow();
+    expect(() => routeMatcher('/((?:.))', '/')).not.toThrow();
+  });
+
+  test('Random', () => {
+    expect(routeMatcher('/icon-:size(\\d+).png', '/icon-24.png')).toEqual([
+      {
+        type: 'DYNAMIC',
+        params: [{ name: 'size', value: '24' }],
+      },
+    ]);
+    expect(routeMatcher('/:foo/(.*)', '/test/route')).toEqual([
+      {
+        type: 'DYNAMIC',
+        params: [{ name: 'foo', value: 'test' }],
+      },
+      {
+        type: 'DYNAMIC',
+        params: [],
+      },
+    ]);
+    expect(
+      routeMatcher('/(apple-)?icon-:res(\\d+).png', '/apple-icon-240.png')
+    ).toEqual([
+      {
+        type: 'DYNAMIC',
+        params: [{ name: 'res', value: '240' }],
+      },
+    ]);
+    expect(
+      routeMatcher('/(apple-)?icon-:res(\\d+).png', '/icon-240.png')
+    ).toEqual([
+      {
+        type: 'DYNAMIC',
+        params: [{ name: 'res', value: '240' }],
+      },
+    ]);
+    expect(
+      routeMatcher('/:remote([\\w-.]+)/:user([\\w-]+)', '/endpoint/user-name')
+    ).toEqual([
+      {
+        type: 'DYNAMIC',
+        params: [
+          {
+            name: 'remote',
+            value: 'endpoint',
+          },
+        ],
+      },
+      {
+        type: 'DYNAMIC',
+        params: [
+          {
+            name: 'user',
+            value: 'user-name',
+          },
+        ],
+      },
+    ]);
+    expect(routeMatcher('/:foo+baz', '/foobaz')).toEqual([
+      {
+        type: 'DYNAMIC',
+        params: [
+          {
+            name: 'foo',
+            value: 'foo',
+          },
+        ],
+      },
+    ]);
+    expect(
+      routeMatcher('/:postType(video|audio|text)(\\+.+)?', '/video')
+    ).toEqual([
+      {
+        type: 'DYNAMIC',
+        params: [
+          {
+            name: 'postType',
+            value: 'video',
+          },
+        ],
+      },
+    ]);
+    expect(routeMatcher('/user(s)?/:user', '/user/abc')).toEqual([
+      {
+        type: 'DYNAMIC',
+        params: [],
+      },
+      {
+        type: 'DYNAMIC',
+        params: [
+          {
+            name: 'user',
+            value: 'abc',
+          },
+        ],
+      },
+    ]);
+    expect(routeMatcher('/user(s)?/:user', '/users/abc')).toEqual([
+      {
+        type: 'DYNAMIC',
+        params: [],
+      },
+      {
+        type: 'DYNAMIC',
+        params: [
+          {
+            name: 'user',
+            value: 'abc',
+          },
+        ],
+      },
+    ]);
   });
 });
--- packages/route-matcher/src/routeMatcher.ts
+++ packages/route-matcher/src/routeMatcher.ts
@@ -1,140 +1,189 @@
-export type SegmentType =
-  | 'STATIC'
-  | 'DYNAMIC_NAMED'
-  | 'DYNAMIC_UNNAMED'
-  | 'EXACT';
+export type SegmentType = 'STATIC' | 'DYNAMIC' | 'EXACT';
 
 export type RouteSegment = {
   type: SegmentType;
-  name: string;
-  value: string | undefined;
+  params: Record<string, string>[];
 };
 
 type ParsedSegment = {
   type: SegmentType;
   pattern: string;
-  name: string;
+  params: string[];
 };
 
-function toRegex(str: string, modifier: string | false) {
-  let name = '';
+function matchCustomRegexp(str: string) {
   let pattern = '';
+  let open = 1;
+  const s = str.slice(1);
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      if (s[i + 1] !== '?') {
+        throw new Error(
+          'Capturing groups are not allowed inside the custom regular expressions.'
+        );
+      }
+      open++;
+    }
+
+    if (s[i] === ')') {
+      open--;
+    }
+
+    if (open === 0) {
+      break;
+    }
+
+    if (s[i] === '\\') {
+      pattern += '\\';
+      continue;
+    }
+
+    pattern += s[i];
+  }
+
+  if (!pattern) {
+    throw new Error(
+      'Missing pattern: custom regular expressions must not be empty.'
+    );
+  }
+
+  return pattern;
+}
+
+function isModifier(c: string) {
+  return ['*', '+', '?'].includes(c);
+}
+
+function toRegex(str: string) {
+  const params: string[] = [];
+
+  let pattern = '/';
 
   for (let i = 0; i < str.length; i++) {
+    // Parse named params w/ or w/o custom regexp
     if (str[i] === ':') {
-      const res = str.slice(i + 1).match(/([a-zA-Z0-9-]+)(?:.*)/);
-      name = res?.[1] as string;
+      const res = str.slice(i + 1).match(/([a-zA-Z0-9_]+)(?:.*)/);
+      const name = res?.[1] as string;
+      params.push(name);
       i += name.length;
-      if (str[i + 1] !== '(') {
-        pattern += '([^/]+)';
+      let currentPattern;
+
+      if (str[i + 1] === '(') {
+        const pat = matchCustomRegexp(str.slice(i + 1));
+        currentPattern = '(' + pat + ')';
+        i += pat.length + 2;
+      } else {
+        currentPattern = '([^/]+)';
       }
+
+      if (isModifier(str[i + 1])) {
+        if (str[i + 1] === '?') {
+          currentPattern = `(?:${currentPattern})?`;
+        } else {
+          currentPattern = `(${currentPattern}(?:(?:/)?(?:${currentPattern}))*)${
+            str[i + 1]
+          }`;
+        }
+
+        i += 1;
+      }
+
+      pattern += currentPattern;
+
       continue;
     }
 
+    // Parse unnamed params with custom regexp
     if (str[i] === '(') {
-      const res = str.slice(i).match(/(?:\((.+)\))/);
-      pattern += `(${res?.[1]})`;
-      i += (res?.[1] as string).length + 1;
+      let currentPattern;
+
+      const pat = matchCustomRegexp(str.slice(i));
+      currentPattern = '(?:' + pat + ')';
+      i += pat.length + 1;
+
+      if (isModifier(str[i + 1])) {
+        if (str[i + 1] === '?') {
+          currentPattern = `(?:${currentPattern})?`;
+        } else {
+          currentPattern = `(${currentPattern}(?:(?:/)?(?:${currentPattern}))*)${
+            str[i + 1]
+          }`;
+        }
+
+        i += 1;
+      }
+
+      pattern += currentPattern;
+
       continue;
     }
 
-    if (str[i] === '*') {
-      pattern += '(?:.*)';
-      continue;
+    if (isModifier(str[i])) {
+      throw new Error(
+        `Unexpected modifier (${str[i]}): Modifiers must be placed after the parameter.`
+      );
     }
 
     pattern += str[i];
   }
 
-  if (modifier) {
-    pattern = `(/${pattern}(?:(?:/)?(?:${pattern}))*)${modifier}`;
-  } else {
-    pattern = '/' + pattern;
-  }
-
-  return { pattern, name };
-}
-
-function getModifier(seg: string) {
-  const modifier = seg.at(-1);
-
-  if (modifier && ['*', '+', '?'].includes(modifier)) {
-    if (seg.includes(':') || seg.includes('(')) {
-      return modifier;
-    }
-
-    throw new Error(
-      `Unexpected modifier (${modifier}), Modifiers must be placed after the parameter.`
-    );
-  } else {
-    return false;
-  }
+  return { pattern, params };
 }
 
 function parseSegment(seg: string): ParsedSegment {
-  let type: SegmentType = 'STATIC';
-  let name = '';
-  let pattern = `/(${seg})`;
-  const modifier = getModifier(seg);
-  const segment = modifier ? seg.slice(0, -1) : seg;
+  const out: ParsedSegment = {
+    params: [],
+    pattern: `/(?:${seg})`,
+    type: 'STATIC',
+  };
 
-  if (seg.includes(':')) {
-    type = 'DYNAMIC_NAMED';
-    const res = toRegex(segment, modifier);
-    pattern = res.pattern;
-    name = res.name;
-  } else if (new RegExp(/[*(]/).test(segment)) {
-    type = 'DYNAMIC_UNNAMED';
-    const res = toRegex(segment, modifier);
-    pattern = res.pattern;
+  if (new RegExp(/[:(*+?]/).test(seg)) {
+    out.type = 'DYNAMIC';
+    const res = toRegex(seg);
+    out.pattern = res.pattern;
+    out.params = res.params;
   }
 
-  return { pattern, type, name };
+  return out;
 }
 
 export default function routeMatcher(
   route: string,
   path: string
 ): RouteSegment[] | null {
-  console.log('Route: ' + route + ' ' + 'Path: ' + path);
-
   if (!route || !path) return null;
 
   if (route === path.toLowerCase()) {
-    return [{ name: '', type: 'EXACT', value: path }] as RouteSegment[];
+    return [{ type: 'EXACT', params: [] }] as RouteSegment[];
   }
 
   const segments = route.split('/').splice(1);
-  console.log('Segments', segments);
-
   const parsedSegments = segments.map(parseSegment);
-  console.log('Parsed Segments', parsedSegments);
-
   const regexp = new RegExp(
     '^' + parsedSegments.map((s) => s.pattern).join('') + '$',
     'i'
   );
-  console.log('Regexp', regexp);
-
   const execArr = regexp.exec(path);
-  console.log('Exec Array', execArr);
 
   if (!execArr) {
     return null;
   }
 
   const output = [];
+  let paramsIndex = 1;
 
   for (let i = 0; i < parsedSegments.length; i++) {
     const obj: RouteSegment = {
       type: parsedSegments[i].type as SegmentType,
-      name: parsedSegments[i].name,
-      value: execArr[1 + i],
+      params: parsedSegments[i].params.map((p) => {
+        const param = { name: p, value: execArr[paramsIndex] };
+        paramsIndex++;
+        return param;
+      }),
     };
     output.push(obj);
   }
-
-  console.log('Output', output);
 
   return output;
 }
